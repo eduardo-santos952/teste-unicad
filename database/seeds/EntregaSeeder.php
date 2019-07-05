d2d25a51112423333115e9b8621e0979ac347d6c
eduardo-santos952 <duudu952@gmail.com>
2019-07-05 07:36:13 -0300

    Gerando seed

---
+++ database/seeds/EntregaSeeder.php
@@ -0,0 +1,182 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EntregaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das neves',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das joias',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das abelhas',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das doidas',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das aaaa',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das bbbb',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das cccc',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das dddd',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das eeee',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das ffff',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das ggggg',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das hhhhh',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das jjjjj',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das xcvvbv',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das fgdfgdfg',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das ewrwerwe',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das 21312',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das sdafsdf',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das nsdsfsdf',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das fdvdfvdfqw',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das sdcsdcsd',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das dfsfsdfsd',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das sdfsdfsd',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+
+        DB::table('entregas')->insert([
+            'nome'         => 'Jao das sdfsdf',
+            'data_entrega' => '2019-09-12',
+            'origem'       =>  84.000666,
+            'destino'      => -85.959595
+        ]);
+    }
+}
