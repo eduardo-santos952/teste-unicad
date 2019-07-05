3ec4e9e76145df86eecab8626b33fd787ee08320
eduardo-santos952 <duudu952@gmail.com>
2019-07-05 06:48:48 -0300

    Allow Origin fix

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/', function(){
+    return 'oi';
+});
 Route::get('/entregas', 'ControladorEntrega@index');
 Route::post('/cadastro_entrega', 'ControladorEntrega@store');
 Route::get('/visualizar_entrega/{id}', 'ControladorEntrega@show');
